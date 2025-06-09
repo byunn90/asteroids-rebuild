35c77c5755bcf3d7240b2e54e02d8905baa4c4cc
byunn90 <kayhanturk90@Live.com>
2025-06-09 14:11:46 +1000

    initialised pygame setup

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT), flags=0, depth=0, display=0, vsync=0)
-    for event in pygame.event.get():
-       if event.type == pygame.QUIT:
-           return
+    pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+       for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+             return
+         screen.fill("black")
+         pygame.display.flip()
 if __name__ == "__main__":
     main()
