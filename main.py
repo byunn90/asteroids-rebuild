47e826a4f73752fc7675f21cb3ac88d13bc37df8
byunn90 <kayhanturk90@Live.com>
2025-06-10 13:02:56 +1000

    player fps

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = clock(60) / 1000
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
        for event in pygame.event.get():
@@ -12,5 +14,6 @@ def main():
              return
          screen.fill("black")
          pygame.display.flip()
+         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
