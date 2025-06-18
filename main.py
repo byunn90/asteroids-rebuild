5e3a3d97deeb2f555934efdaa1893df88a973fa0
byunn90 <kayhanturk90@Live.com>
2025-06-18 19:24:18 +1000

    overriding methods using polymorphism

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
@@ -16,12 +21,14 @@ def main():
          if event.type == pygame.QUIT:
              return
 
-         player.update(dt)
-
+         updatable.update(dt)
          screen.fill("black")
-         player.draw(screen)
-         pygame.display.flip()
+
          dt = clock.tick(60) / 1000
+
+         for obj in drawable:
+            obj.draw(screen)
+            pygame.display.flip()  
 
 if __name__ == "__main__":
     main()
